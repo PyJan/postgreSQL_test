1f72ee404c82b00af12762f13602639d9f62eedd
PyJan <jan.svarcbach@gmail.com>
2019-02-24 21:38:55 +0100

    save car info in db

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sammy:sammy@localhost/sammy'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sammy:sammy@localhost:5432/sammy'
 db = SQLAlchemy(app)
 
 
@@ -10,9 +10,13 @@ class car(db.Model):
     name = db.Column(db.String(50), primary_key=True)
     year = db.Column(db.Integer)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def main():
-    return '<h1>this is main page</h1>'
+    if request.method == 'POST':
+        mycar = car(name=request.form['car'], year=request.form['year'])
+        db.session.add(mycar)
+        db.session.commit()
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
